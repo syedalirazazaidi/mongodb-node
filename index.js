e689418e31a7c3fd94ec5723b44ae280b05af17f
DESKTOP-7V9Q99B\AKU <akuraza6atgmail.com>
2023-08-25 18:53:12 +0500

    connection successfully with mongodb

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 require('dotenv').config()
+const express = require('express')
 const connectDB = require('./db/connect')
 
+const app = express()
+app.use(express.json())
 
 const PORT = process.env.PORT | 5000
 
